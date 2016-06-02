5799e06209820f7eacea974d8165cfc0280b8dae
DPP <dpeczek93@gmail.com>
2016-06-02 20:58:01 +0200

    Add grayscale conversion, fast segmentation

--- EyeMouthDetection/ClassificationWithSegmentationMethod/src/main/java/method/main/Main.java
+++ EyeMouthDetection/ClassificationWithSegmentationMethod/src/main/java/method/main/Main.java
@@ -1,11 +1,27 @@
 package method.main;
 
+import method.utils.ColorModelModifier;
+import method.utils.Point;
+import segmentation.SegmentationRegionGrowth;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Daniel on 2016-05-28.
  */
 public class Main {
-    public static void main(String args[]){
+    public static void main(String args[]) throws IOException {
         System.out.println("Hello");
         System.out.println(Main.class.getPackage());
+
+        BufferedImage b = ImageIO.read(new File("mike.jpg"));
+        b = ColorModelModifier.convertImageToGrayscale(b);
+        ImageIO.write(b, "jpg", new File("garyWhite.jpg"));
+        b = SegmentationRegionGrowth.segmentateImage(b, 30, new Point(60, 46));
+
+        ImageIO.write(b, "jpg", new File("garyTest.jpg"));
     }
 }
--- EyeMouthDetection/ClassificationWithSegmentationMethod/src/main/java/segmentation/SegmentationRegionGrowth.java
+++ EyeMouthDetection/ClassificationWithSegmentationMethod/src/main/java/segmentation/SegmentationRegionGrowth.java
@@ -4,6 +4,7 @@ import method.utils.Point;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
@@ -20,12 +21,72 @@ public class SegmentationRegionGrowth {
      */
     public static BufferedImage segmentateImage(BufferedImage image, double edge, Point startingPoint){
 
+
+
         List<Point> segmentatedPoints = new ArrayList<>();
-        List<Point> wrongPoints = new ArrayList<>();
+        List<Point> visitedPoints = new ArrayList<>();
 
+        Queue<Point> pointsToCheck = new LinkedList<>();
 
+        segmentatedPoints.add(startingPoint);
+        visitedPoints.add(startingPoint);
+        double segmentatedPixelCounter = 1.0 ;
+        double [] pixelTab = new double[image.getColorModel().getPixelSize()/8];
+        pixelTab = image.getRaster().getPixel(startingPoint.getX(), startingPoint.getY(), pixelTab);
+        double sum = pixelTab[0];
+        double average = 0;
+        pointsToCheck.addAll(getNeighbourPoints(image.getWidth(), image.getHeight(), startingPoint));
+        while (segmentatedPixelCounter < 100) {
+
+            if (visitedPoints.contains(pointsToCheck.element())){
+                pointsToCheck.poll();
+            }
+            Point p = pointsToCheck.element();
+            visitedPoints.add(pointsToCheck.element());
+            pixelTab = new double[image.getColorModel().getPixelSize()/8];
+            pixelTab = image.getRaster().getPixel(p.getX(), p.getY(), pixelTab);
+            average = sum / segmentatedPixelCounter;
+
+            if (pixelTab[0] <= average + edge && pixelTab[0] >= average - edge){
+                System.out.println(pixelTab[0] + " " + average);
+                segmentatedPoints.add(pointsToCheck.element());
+                segmentatedPixelCounter++;
+                sum += pixelTab[0];
+                pointsToCheck.addAll(getNeighbourPoints(image.getWidth(), image.getHeight(), pointsToCheck.remove()));
+
+            }
+        }
+
+        pixelTab = new double[image.getColorModel().getPixelSize()/8];
+        for (int i = 0; i < pixelTab.length; i++){
+            pixelTab[i] = 255;
+        }
+        for (Point p : segmentatedPoints){
+            image.getRaster().setPixel(p.getX(), p.getY(), pixelTab);
+        }
+
+        visitedPoints.clear();
+        segmentatedPoints.clear();
 
         return image;
+    }
+
+    private static List<Point> getNeighbourPoints(int imageWidth, int imageHeight, Point point){
+        List<Point> list = new ArrayList<>();
+        if(point.getX()+1 < imageWidth){
+            list.add(new Point(point.getX()+1, point.getY()));
+        }
+        if(point.getX() - 1 >= 0){
+            list.add(new Point(point.getX()-1, point.getY()));
+        }
+        if(point.getY() + 1 < imageHeight){
+            list.add(new Point(point.getX(), point.getY() +1));
+        }
+        if (point.getY() - 1 >= 0){
+            list.add(new Point(point.getX(), point.getY() - 1));
+        }
+
+        return list;
     }
 
 }
