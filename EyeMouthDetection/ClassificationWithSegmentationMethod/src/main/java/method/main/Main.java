718bfa04561efc9b8ebd994ea6b5dd51dae106e0
DPP <dpeczek93@gmail.com>
2016-06-03 09:37:59 +0200

    Add computing integral image

--- EyeMouthDetection/ClassificationWithSegmentationMethod/src/main/java/method/main/Main.java
+++ EyeMouthDetection/ClassificationWithSegmentationMethod/src/main/java/method/main/Main.java
@@ -1,6 +1,7 @@
 package method.main;
 
 import method.utils.ColorModelModifier;
+import method.utils.IntegralImage;
 import method.utils.Point;
 import segmentation.SegmentationRegionGrowth;
 
@@ -21,7 +22,30 @@ public class Main {
 //        b = ColorModelModifier.convertImageToGrayscale(b);
         ImageIO.write(b, "jpg", new File("garyWhite.jpg"));
 //        b = SegmentationRegionGrowth.segmentateImageByGrayscale(b, 17, new Point(102, 117));
-        b = SegmentationRegionGrowth.segmentateImageByGrayscale(b, 15, new Point(102, 117));
+        IntegralImage ii = new IntegralImage(b);
+
+        int diff1 = 10;
+        int diff2 = 5;
+
+        int x = 146;
+        int y = 110;
+
+        Point p1a = new Point(x-diff1,y-diff1);
+        Point p1b = new Point(x+diff1,y-diff1);
+        Point p1c = new Point(x-diff1,y+diff1);
+        Point p1d = new Point(x+diff1,y+diff1);
+
+        Point p2a = new Point(x-diff2,y-diff2);
+        Point p2b = new Point(x+diff2,y-diff2);
+        Point p2c = new Point(x-diff2,y+diff2);
+        Point p2d = new Point(x+diff2,y+diff2);
+
+
+        int sum1 = ii.getSum(p1a, p1b, p1c, p1d);
+        int sum2 = ii.getSum(p2a, p2b, p2c, p2d);
+        System.out.println("sum1 = " + sum1);
+        System.out.println("sum2 = " + sum2);
+        System.out.println(sum1 - sum2);
 
         ImageIO.write(b, "jpg", new File("garyTest.jpg"));
     }
---
+++ EyeMouthDetection/ClassificationWithSegmentationMethod/src/main/java/method/utils/IntegralImage.java
@@ -0,0 +1,69 @@
+package method.utils;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by dpp on 6/3/16.
+ */
+public class IntegralImage {
+
+    private int[][] summedAreaTable;
+
+    public IntegralImage(BufferedImage grayscaleImage){
+        setupSummedAreaTable(grayscaleImage);
+    }
+
+    private void setupSummedAreaTable(BufferedImage image){
+        summedAreaTable = new int[image.getWidth()][image.getHeight()];
+        int[] pixelValue = new int[image.getColorModel().getPixelSize()/8];
+        int value;
+        for (int x = 0; x < image.getWidth(); x++){
+            for (int y = 0; y < image.getHeight(); y++){
+                value = image.getRaster().getPixel(x,y,pixelValue)[0];
+                if (x == 0 && y == 0){
+                    summedAreaTable[x][y] = value;
+                    continue;
+                }
+
+                if (x > 0 && y == 0){
+                    value += summedAreaTable[x - 1][y];
+                    summedAreaTable[x][y] = value;
+                    continue;
+                }
+
+                if (x == 0 && y > 0){
+                    value += summedAreaTable[x][y - 1];
+                    summedAreaTable[x][y] = value;
+                    continue;
+                }
+
+                value += summedAreaTable[x - 1][y];
+                value += summedAreaTable[x][y - 1];
+                value -= summedAreaTable[x - 1][y - 1];
+                summedAreaTable[x][y] = value;
+            }
+        }
+
+    }
+
+    public int[][] getSummedAreaTable() {
+        return summedAreaTable;
+    }
+
+    /**
+     *
+     * @param a left top corner
+     * @param b right top corner
+     * @param c left bottom corner
+     * @param d right bottom corner
+     * @return
+     */
+    public int getSum(Point a, Point b, Point c, Point d){
+        int sum = 0;
+        sum += summedAreaTable[(int) a.getX()][(int) a.getY()];
+        sum += summedAreaTable[(int) d.getX()][(int) d.getY()];
+        sum -= summedAreaTable[(int) b.getX()][(int) b.getY()];
+        sum -= summedAreaTable[(int) c.getX()][(int) c.getY()];
+        return sum;
+    }
+}
